1219189210dd9124c816decda9000b98b26a2822
현태수 <qvil@op.gg>
2019-09-25 21:49:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next.ts Boilerplate
+
+Light and choosable Next.js boilerplate with Typescript.
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+interface IHomeProps {}
+
+const Home: NextPage<IHomeProps> = () => {
+  return <div>Hello Next.ts boilerplate</div>;
+};
+
+export default Home;
